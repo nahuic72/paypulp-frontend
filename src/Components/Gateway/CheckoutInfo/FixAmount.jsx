898332f97787d4bba1fbab1e279e3e4e936cd76a
DanNisenson <danpablo1@gmail.com>
2023-03-16 14:02:34 +0100

    👷Create fix amount component

---
+++ src/Components/Gateway/CheckoutInfo/FixAmount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FixAmount = () => {
+  return (
+    <div className="checkout__info">
+      <Seller />
+      <div className="checkout__final-amount">10 e</div>
+    </div>
+  )
+}
+
+export default FixAmount
