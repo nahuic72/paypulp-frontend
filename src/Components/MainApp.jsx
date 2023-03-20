9688af9c6a18e240b3c911f2f3ba7a49810b55d2
DanNisenson <danpablo1@gmail.com>
2023-03-20 18:39:33 +0100

    🗄Move Toaster component to MainApp & change spelling error

--- src/Components/MainApp.jsx
+++ src/Components/MainApp.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from 'react-hot-toast'
 import { Outlet } from 'react-router-dom'
 
 const MainApp = () => {
   return (
     <main>
       <Outlet />
+      <Toaster />
     </main>
   )
 }
