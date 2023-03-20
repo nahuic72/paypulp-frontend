319e859b8857c12ce20c85690e20811ce04582f4
DanNisenson <danpablo1@gmail.com>
2023-03-20 19:15:00 +0100

    🔨Fix undefined variable error if no validation required

--- src/Components/Elements/TextInput.jsx
+++ src/Components/Elements/TextInput.jsx
@@ -1,16 +1,15 @@
 import 'Styles/Inputs.css'
 
 const TextInput = ({ name, label, register, validationType, errors }) => {
-  console.log(errors)
   return (
     <div>
       <div className="text-input__wrapper">
         <label htmlFor={name}>{label}</label>
-        <input type="text" name={name} {...register(name, validationType)} />
+        <input type="text" {...register(name, validationType)} />
         {name === 'email' && <div>Email MUI</div>}
         {name === 'password' && <div>password icon</div>}
       </div>
-      {errors[name] && <p className="text-input__error">{errors[name].message}</p>}
+      {errors?.[name] && <p className="text-input__error">{errors?.[name].message}</p>}
     </div>
   )
 }
