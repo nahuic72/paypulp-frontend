fae384f879c5ba2145f96eff5cabc60907db3627
DanNisenson <danpablo1@gmail.com>
2023-03-20 21:51:15 +0100

    🎨Add fonts, basic elements styling and CSS vars

--- src/Pages/Private/Home.jsx
+++ src/Pages/Private/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <h1 >Home</h1>
+      <h2 >Home</h2>
+      <h3 >Home</h3>
+      <div className="text">Home</div>
+      <div className="link">Home</div>
+      <div className="text-aux">Home</div>
+      <div className="link-aux">Home</div>
+    </>
+  )
 }
 
 export default Home
