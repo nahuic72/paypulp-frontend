71a178c5840db25cf12fea2cda4db47cf166472a
DanNisenson <danpablo1@gmail.com>
2023-03-20 11:19:01 +0100

    ✨Disable pay btn on click

--- src/Components/Gateway/Checkout.jsx
+++ src/Components/Gateway/Checkout.jsx
@@ -1,4 +1,5 @@
 import CardImage from 'Components/Elements/CardImage'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import 'Styles/Checkout.css'
 import FixAmount from './CheckoutInfo/FixAmount'
@@ -6,6 +7,12 @@ import FixAmount from './CheckoutInfo/FixAmount'
 const Checkout = ({ transactionInfo, payMets, confirmTransaction }) => {
   const navigate = useNavigate()
   const { checkoutType, totalAmount } = transactionInfo
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  const pay = () => {
+    setIsDisabled(true)
+    confirmTransaction()
+  }
 
   const goToApp = () => navigate('/login')
 
@@ -20,7 +27,7 @@ const Checkout = ({ transactionInfo, payMets, confirmTransaction }) => {
         )}
       </div>
       <div className="checkout__btns">
-        <button className="btn btn-solid btn-long" onClick={() => confirmTransaction()}>
+        <button className="btn btn-solid btn-long" onClick={pay} disabled={isDisabled}>
           PAGAR
         </button>
         <button className="btn btn-text-only btn-short" onClick={goToApp}>
