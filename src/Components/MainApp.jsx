c0e33ae55fd75f7d744ad221d83fc374dbbef34a
DanNisenson <danpablo1@gmail.com>
2023-03-20 13:26:09 +0100

    🧹Cleanup component

--- src/Components/MainApp.jsx
+++ src/Components/MainApp.jsx
@@ -1,13 +1,9 @@
-import useTokenValidation from 'Hooks/useTokenValidation'
 import { Outlet } from 'react-router-dom'
 
 const MainApp = () => {
-  // const { tokenError, setTokenError } = useTokenValidation()
-
   return (
     <main>
       <Outlet />
-      {/* toast */}
     </main>
   )
 }
