c223fc853c2b695491e9fc03a581502d35443e92
DanNisenson <danpablo1@gmail.com>
2023-03-22 15:30:14 +0100

    🧹Cleanup context hook

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -2,24 +2,12 @@ import { createContext, useState } from 'react'
 
 export const userContext = createContext(null)
 
-const UserContext = ({ children }) => {
-  const [userInfo, setUserInfo] = useState({})
-  const [transactions, setTransactions] = useState([])
-  const [paymentMethods, setPaymentMethods] = useState([])
+const UserCtxt = ({ children }) => {
+  const [userContext, setUserContext] = useState({})
 
   return (
-    <userContext.Provider
-      value={{
-        userInfo,
-        setUserInfo,
-        transactions,
-        setTransactions,
-        paymentMethods,
-        setPaymentMethods,
-      }}>
-      {children}
-    </userContext.Provider>
+    <userContext.Provider value={{ userContext, setUserContext }}>{children}</userContext.Provider>
   )
 }
 
-export default UserContext
+export default UserCtxt
