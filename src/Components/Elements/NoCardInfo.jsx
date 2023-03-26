c6a702d31938b662fb732402a57e94f4b52a8a37
Golde Tischler <golde.tischler@gmail.com>
2023-03-26 14:02:28 +0200

    No card info component

---
+++ src/Components/Elements/NoCardInfo.jsx
@@ -0,0 +1,16 @@
+import 'Styles/NoCardInfo.css'
+
+const NoCardInfo = () => {
+  return (
+    <>
+      <section className="no-card__container">
+        <h3 className="no-card__text">
+          No tienes tarjetas asociadas. Carga los datos de tus tarjetas y ahorra tiempo cuando
+          quieras realizar un pago.
+        </h3>
+      </section>
+    </>
+  )
+}
+
+export default NoCardInfo
--- src/Components/Home/HomeHeader.jsx
+++ src/Components/Home/HomeHeader.jsx
@@ -5,7 +5,7 @@ const Header = ({ name }) => {
   return (
     <div className="home-header">
       <img className="home-header__avatar" src={avatar} alt="Avatar" />
-      <div className="home-header__greeting">Hola {name}</div>
+      <h3 className="home-header__greeting">Hola {name}</h3>
       <div className="home-header__help">?</div>
     </div>
   )
