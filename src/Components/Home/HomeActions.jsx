c71bdd3baaf8a6e913a7ce5faaab9c316c9c669b
Golde Tischler <golde.tischler@gmail.com>
2023-03-27 13:56:22 +0200

    pay with qr pge

--- src/Components/Home/HomeActions.jsx
+++ src/Components/Home/HomeActions.jsx
@@ -8,11 +8,17 @@ const HomeActions = ({ accountType }) => {
     navigate(url)
   }
 
+  const payWithQR = () => {
+    navigate('/paywithqr')
+  }
+
   return (
     <div className="home-actions">
       <button className="btn btn-long btn-solid">Añadir método de pago</button>
       <div className="home-actions__bottom-btns">
-        <div className="home-actions__bottom-btn">Pagar con QR</div>
+        <div className="home-actions__bottom-btn" onClick={payWithQR}>
+          Pagar con QR
+        </div>
         <div className="home-actions__bottom-btn" onClick={() => goToQrGen(`fixed`)}>
           Cobrar con QR
         </div>
