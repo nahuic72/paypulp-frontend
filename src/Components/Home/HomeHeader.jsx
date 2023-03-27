8b88394a4f9b7873705438007baf579dcf177f53
DanNisenson <danpablo1@gmail.com>
2023-03-27 12:31:34 +0200

    🎨Add icons @ Home

--- src/Components/Home/HomeHeader.jsx
+++ src/Components/Home/HomeHeader.jsx
@@ -1,12 +1,15 @@
 import 'Styles/Home.css'
 import avatar from 'Assets/Images/Avatar.png'
+import QuestionMark from 'Components/Elements/Icons/QuestionMark'
 
 const Header = ({ name }) => {
   return (
     <div className="home-header">
       <img className="home-header__avatar" src={avatar} alt="Avatar" />
       <div className="home-header__greeting">Hola {name}</div>
-      <div className="home-header__help">?</div>
+      <div className="home-header__help">
+        <QuestionMark />
+      </div>
     </div>
   )
 }
