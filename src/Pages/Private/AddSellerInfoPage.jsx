5b165963bbc9137b695774b33a64a4cd142746dd
Golde Tischler <golde.tischler@gmail.com>
2023-03-27 22:59:46 +0200

    comps for svgs &added nav to home for left arrow

--- src/Pages/Private/AddSellerInfoPage.jsx
+++ src/Pages/Private/AddSellerInfoPage.jsx
@@ -1,10 +1,16 @@
 import AddSellerForm from 'Components/AddSellerInfo/AddSellerForm'
 import TextHeader from 'Components/Elements/TextHeader'
+import { useNavigate } from 'react-router'
 
 const AddSellerInfoPage = () => {
+  const navigate = useNavigate()
+  const goBack = () => {
+    navigate('/home')
+  }
+
   return (
     <div>
-      <TextHeader text="Datos de vendedor" />
+      <TextHeader text="Datos de vendedor" navigate={goBack} />
       <AddSellerForm />
     </div>
   )
